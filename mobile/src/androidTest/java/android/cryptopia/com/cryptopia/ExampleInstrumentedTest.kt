45cc217b00c2673d76ace564330dc614a58d1172
Robert Zhang <robertsky.zhang@gmail.com>
2017-11-18 10:46:06 -0500

    created project

---
+++ mobile/src/androidTest/java/android/cryptopia/com/cryptopia/ExampleInstrumentedTest.kt
@@ -0,0 +1,19 @@
+package android.cryptopia.com.cryptopia
+
+import android.support.test.InstrumentationRegistry
+import android.support.test.runner.AndroidJUnit4
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getTargetContext()
+        assertEquals("android.cryptopia.com.cryptopia", appContext.packageName)
+    }
+}
